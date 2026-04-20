f05317db7acc70638457e73d67ee14874901de59
spvieira <spgva@proton.me>
2026-04-20 21:04:25 +0200

    Cleaning up notebook + more results

--- plot.py
+++ plot.py
@@ -48,4 +48,45 @@ def plot_change_count():
     plt.tight_layout()
     plt.savefig('change_count_no_rsc.png')
 
-plot_change_count()
+def plot_classifications_separate():
+    df = pd.read_csv('Results/ClassificationsDAA20_25.csv', index_col=0)
+
+    years = df.index.tolist()
+
+    for year in years:
+        row = df.loc[year]
+        total_sum = row.values.sum()
+        percentage = (row.values/total_sum) * 100
+        bars = plt.bar(row.index, percentage, color='blue')
+        plt.title(f'Land Cover Distribution - {year}', fontweight='bold')
+        plt.ylabel('Percentage')
+        plt.ylim(0, percentage.max() * 1.15)
+        plt.bar_label(bars, fmt='%.2f%%', padding=3)
+        plt.tick_params(axis='x', rotation=45)
+
+        plt.tight_layout()
+        plt.savefig('classifications_daa'+ str(year) + '.png')
+        plt.clf() 
+
+def plot_classifications_comparison():
+    df = pd.read_csv('Results/ClassificationsDAA20_25.csv', index_col=0)
+    df_percent = df.div(df.sum(axis=1), axis=0) * 100
+
+    ax = df_percent.T.plot(kind='bar', figsize=(16, 8), edgecolor='black', width=0.85)
+    for container in ax.containers:
+        ax.bar_label(container, fmt='%.1f%%', padding=3, fontsize=8, rotation=90)
+
+    plt.title('Comparison of Land Cover Categories (2020-2025)')
+    plt.ylabel('Value')
+    plt.xlabel('Category')
+    plt.xticks(rotation=45)
+    plt.legend(title='Year')
+
+    plt.legend(title='Year', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    # Adjust y-limit to provide space for vertical labels
+    plt.ylim(0, df_percent.max().max() * 1.2)
+    plt.tight_layout()
+    plt.savefig('classifications_daa_comparison.png')
+
+plot_change_matrix(22)
